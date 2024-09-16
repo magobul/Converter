16e6a97c8533bf450c003bad850dd7302daa27c2
magobul <magobul@gmail.com>
2024-09-16 10:25:43 +0700

    Add files via upload

---
+++ peregruzka/Overload.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace peregruzka
+{
+    internal class Overload
+    {
+        public void Convert(string input, out int result)
+        {
+            int.TryParse(input, out result);
+        }
+
+        public void Convert(string input, out double result)
+        {
+            double.TryParse(input, out result);
+        }
+
+        public void Convert(string input, out bool result)
+        {
+            bool.TryParse(input, out result);
+        }
+    }
+}
---
+++ peregruzka/Program.cs
@@ -0,0 +1,18 @@
+﻿using peregruzka;
+
+class Program
+{
+    static void Main()
+    {
+        Overload converter = new Overload();
+
+        converter.Convert("123", out int intResult);
+        Console.WriteLine($"Конвертация в int: {intResult}");
+
+        converter.Convert("123.45", out double doubleResult); ;
+        Console.WriteLine($"Конвертация в double: {doubleResult}");
+
+        converter.Convert("true", out bool boolResult);
+        Console.WriteLine($"Конвертация в bool: {boolResult}");
+    }
+}
